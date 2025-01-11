978518df4af542a028eb8fd10e7a59817697fa3c
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2025-01-11 15:57:40 +0300

    Login page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-home',
+    standalone: true,
     imports: [],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
--- src/app/components/layouts/layouts.component.ts
+++ src/app/components/layouts/layouts.component.ts
@@ -1,8 +1,10 @@
+import { Statement } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-layouts',
+    standalone:true,
     imports: [RouterOutlet],
     templateUrl: './layouts.component.html',
     styleUrl: './layouts.component.css'
